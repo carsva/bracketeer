0f91371e80c3b0c694e479abe61d82ba7dd9c212
Carl Svahnstrom <carl@tocco.se>
2018-09-07 16:16:58 +0200

    Added dotabutton and made a better position for buttons with fixed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,35 +6,58 @@ import GrandFinal from './../components/GrandFinal';
 
 class Home extends Component {
   render() {
-
     return (
       <AppConsumer>
         {context => (
           <div>
-          <div className="MasterContainer">
-            <div className="FirstContainer">
-            <UpperBracket/>
-            <LowerBracket/>
-            </div>
-            <div className="SecondContainer">
-            <GrandFinal/>
-            <button className="resolve_button" onClick={context.state.test}>Resolve</button>
-            <button className="flag_button" onClick={context.state.flags}>Flags</button>
-            <button className="clear_button" onClick={context.state.clear}>Clear</button>
-            </div>
+            <div className="MasterContainer">
+              <div className="FirstContainer">
+                <UpperBracket />
+                <LowerBracket />
               </div>
-              <div className="winnerContainer">
+              <div className="SecondContainer">
+                <GrandFinal />
+              </div>
+            </div>
+            <div className="winnerContainer">
               <div className="winner">
-                <h3>{context.state.placing(23).name0 ? context.state.placing(23).name0 : ""}</h3> 
-                <img alt="" src={context.state.placing(23).logo0 ? context.state.placing(23).logo0 : ""}></img>
+                <h3>
+                  {context.state.placing(23).name0
+                    ? context.state.placing(23).name0
+                    : ''}
+                </h3>
+                <img
+                  alt=""
+                  src={
+                    context.state.placing(23).logo0
+                      ? context.state.placing(23).logo0
+                      : ''
+                  }
+                />
               </div>
-              </div>
-              {/* <div>
+            </div>
+            <div className="buttons">
+              <button className="resolve_button" onClick={context.state.test}>
+                Resolve
+              </button>
+              <button className="clear_button" onClick={context.state.clear}>
+                Clear
+              </button>
+
+              <button className="dota_button" onClick={context.state.flags}>
+                Dota teams
+              </button>
+              <button className="flag_button" onClick={context.state.flags}>
+                Flags
+              </button>
+            </div>
+
+            {/* <div>
                 {console.log(context.state)}           
                 <h3>Data that lives in the context</h3>
                 <pre>{JSON.stringify(context, null, 4)}</pre>
               </div> */}
-            </div>
+          </div>
         )}
       </AppConsumer>
     );
